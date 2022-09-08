bb1c82d72052d139302a67590fcdac3af47a0d8c
carlton <carltonmbaja0@gmail.com>
2022-09-08 13:52:59 +0300

    Fetched data in TeamLeads.js & parsed them

--- src/Components/TeamLeads.js
+++ src/Components/TeamLeads.js
@@ -1,41 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Example from './Nav'
 import { Link } from "react-router-dom";
 import { Container, Row } from 'reactstrap'
 import { Button } from 'reactstrap';
 
-function TeamLeads() {
+class TeamLeads extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          customers: []
+      }
+  }
+  componentDidMount() {
+      fetch("http://localhost:9292/team_leads")
+      .then(res => res.json())
+      .then(
+          (customers) => {
+              this.setState({ customers: customers });
+          },
+          (error) => {
+              alert(error);
+          }
+      )
+  }
+
+render() {
+
   return (
     <div>
+      
       {/* Imports Navbar to TeamLeads table */}
       <Example />
       <table class="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">Work Station</th>
+            <th scope="col">Action</th>
+
           </tr>
         </thead>
         <tbody>
+        {this.state.customers.map(customer =>
           <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <th scope="row">{customer.id}</th>
+            <td>{customer.firstname}</td>
+            <td>{customer.lastname}</td>
+            <td>{customer.phone}</td>
+            <td>{customer.email}</td>
+            <td>{customer.work_station}</td>
+
+            <td> <Button color="danger" >Del</Button></td>
           </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+         )}
         </tbody>
       </table>
       <Container className="App">
@@ -47,6 +68,6 @@ function TeamLeads() {
       </Container>
     </div>
   )
+  }
 }
-
 export default TeamLeads
